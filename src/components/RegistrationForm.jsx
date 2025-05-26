22d7c3435ad62d896c3a1770aaae4defa4d36d3e
German World Club <germanworldclub@Germans-Mac-mini.local>
2025-05-26 10:53:20 +0400

    add email duplicate validation

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -4,6 +4,11 @@ import * as Yup from "yup";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from "axios";
+import { FaWhatsapp } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+import { MdDriveFileRenameOutline } from "react-icons/md";
 
 const validationSchema = Yup.object({
     fullName: Yup.string().required("Full Name is required"),
@@ -16,10 +21,11 @@ const validationSchema = Yup.object({
 
 const RegistrationForm = () => {
     const [submitted, setSuccess] = useState(false);
+    const [duplicateRecord, setDuplicateRecord] = useState(false);
+    const [duplicateRecordMessage, setDuplicateRecordMessage] = useState("");
     const server_api = import.meta.env.VITE_SERVER_API
-    const handleSubmit = async (values) => {
+    const handleSubmit = async (values, { setFieldError, setSubmitting }) => {
 
-        console.log(values);
 
 
         const trimmedValues = Object.fromEntries(
@@ -28,14 +34,36 @@ const RegistrationForm = () => {
 
         try {
             const response = await axios.post(`${server_api}/api/forum-registration`, trimmedValues);
-            setSuccess(true)
+            if (response.status === 200) {
+                setSuccess(true)
+                setDuplicateRecord(false);
+                setDuplicateRecordMessage("")
+            }
+
         } catch (error) {
+            if (error.response) {
+
+                if (error.response.status === 409) {
+                    setFieldError("email", "This email has already been taken.");
+                } else {
+                    setMessage(`Failed with status code: ${error.response.status}`);
+                }
+
+            } else if (error.request) {
+                // The request was made but no response was received
+                setMessage("No response from server. Please try again later.");
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                setMessage("Failed to submit registration.");
+            }
+            setSuccess(false);
             console.error(error);
-            setMessage('Failed to submit registration.');
         }
 
     };
+
     const initialValues = {
+        timestamp: new Date().toISOString(),
         fullName: "",
         birthdate: "",
         phoneNumber: "",
@@ -59,18 +87,37 @@ const RegistrationForm = () => {
             <div className={`container-fluid registration-form-container ${submitted ? "hide" : ""}`}>
 
                 <h1 className="mb-4 text-start">Participant Registration – German Forum 2025</h1>
-                <h4 className="mb-4 text-start">Date: 17th June 2025</h4>
+                <h4 className="mb-4 text-start">
+
+                    Date: 17th June 2025
+
+                </h4>
                 <div className="row justify-content-center">
-                    <div className="col-12 col-lg-10"> {/* Full width on desktop, padded on smaller screens */}
+                    <div className="col-12 col-lg-12"> {/* Full width on desktop, padded on smaller screens */}
                         <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
                             {({ values, setFieldValue, errors, touched }) => (
                                 <Form>
+                                    <Field
+                                        name="timestamp"
+                                        type="hidden"
+                                        value={new Date().toISOString()}
+                                    />
                                     {/* Full Name */}
                                     <div className="row mb-3 align-items-center">
                                         <label htmlFor="fullName" className="col-sm-3 col-form-label text-start">Full Name</label>
                                         <div className="col-sm-9">
-                                            <Field name="fullName" type="text"
-                                                className={`form-control ${errors.fullName && touched.fullName ? 'input-error' : ''}`} />
+
+                                            <div className="input-group">
+                                                <span className="input-group-text">
+                                                    <MdDriveFileRenameOutline />
+
+                                                </span>
+
+                                                <Field name="fullName" type="text"
+                                                    className={`form-control ${errors.fullName && touched.fullName ? 'is-invalid' : ''}`} />
+
+                                            </div>
+
                                             <div style={{ minHeight: "22px" }} className="text-start">
 
                                                 <ErrorMessage name="fullName" component="div" className="text-danger small" />
@@ -83,23 +130,34 @@ const RegistrationForm = () => {
                                         <label htmlFor="birthdate" className="col-sm-3 col-form-label text-start">Birthdate</label>
                                         <div className="col-sm-9">
                                             {/* <Field name="birthdate" type="date" className="form-control" /> */}
+                                            <div className="input-group">
+                                                <span className="input-group-text">
+                                                    <MdOutlineCalendarMonth />
 
-                                            <DatePicker
-                                                id="birthdate"
-                                                selected={values.birthdate ? new Date(values.birthdate) : null}
-                                                onChange={(date) => {
-                                                    const formattedDate = date ? date.toISOString().split("T")[0] : "";
-                                                    setFieldValue("birthdate", formattedDate);
-                                                }}
-                                                dateFormat="yyyy-MM-dd"
-                                                placeholderText="yyyy-MM-dd"
-                                                className={`form-control ${errors.fullName && touched.fullName ? 'input-error' : ''}`}
-                                                maxDate={new Date()}
-                                                showMonthDropdown
-                                                showYearDropdown
+                                                </span>
 
-                                                dropdownMode="select"
-                                            />
+                                                <DatePicker
+                                                    id="birthdate"
+                                                    selected={values.birthdate ? new Date(values.birthdate) : null}
+                                                    onChange={(date) => {
+                                                        const formattedDate = date ? date.toISOString().split("T")[0] : "";
+                                                        setFieldValue("birthdate", formattedDate);
+                                                    }}
+                                                    dateFormat="yyyy-MM-dd"
+                                                    placeholderText="yyyy-MM-dd"
+                                                    className={`form-control ${errors.fullName && touched.fullName ? 'is-invalid' : ''}`}
+                                                    maxDate={new Date()}
+                                                    showMonthDropdown
+                                                    showYearDropdown
+
+                                                    dropdownMode="select"
+                                                />
+
+
+                                            </div>
+
+
+
                                             <div style={{ minHeight: "22px" }} className="text-start">
 
                                                 <ErrorMessage name="birthdate" component="div" className="text-danger small" />
@@ -111,10 +169,23 @@ const RegistrationForm = () => {
                                     <div className="row mb-3 align-items-center">
                                         <label htmlFor="phoneNumber" className="col-sm-3 col-form-label text-start">Phone Number</label>
                                         <div className="col-sm-9">
-                                            <Field name="phoneNumber" type="tel"
-                                                className={`form-control ${errors.fullName && touched.fullName ? 'input-error' : ''}`}
-                                                pattern="^(\+?[0-9]{1,3}-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$"
-                                                title="Phone number must be in the format: +1-123-456-7890 or 123-456-7890" />
+
+
+                                            <div className="input-group">
+                                                <span className="input-group-text">
+                                                    <FaPhoneAlt />
+
+                                                </span>
+                                                <Field name="phoneNumber" type="tel"
+                                                    className={`form-control ${errors.fullName && touched.fullName ? 'is-invalid' : ''}`}
+                                                    pattern="^(\+?[0-9]{1,3}-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$"
+                                                    title="Phone number must be in the format: +1-123-456-7890 or 123-456-7890" />
+
+                                            </div>
+
+
+
+
                                             <div style={{ minHeight: "22px" }} className="text-start">
 
                                                 <ErrorMessage name="phoneNumber" component="div" className="text-danger small" />
@@ -126,9 +197,18 @@ const RegistrationForm = () => {
                                     <div className="row mb-3 align-items-center">
                                         <label htmlFor="whatsappNumber" className="col-sm-3 col-form-label text-start">WhatsApp Number</label>
                                         <div className="col-sm-9">
-                                            <Field name="whatsappNumber" type="tel" className={`form-control ${errors.fullName && touched.fullName ? 'input-error' : ''}`} 
-                                                pattern="^(\+?[0-9]{1,3}-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$"
-                                                title="Phone number must be in the format: +1-123-456-7890 or 123-456-7890" />
+                                            <div className="input-group">
+                                                <span className="input-group-text">
+                                                    <FaWhatsapp />
+                                                </span>
+                                                <Field
+                                                    name="whatsappNumber"
+                                                    type="tel"
+                                                    className={`form-control ${errors.whatsappNumber && touched.whatsappNumber ? 'is-invalid' : ''}`}
+                                                    pattern="^(\+?[0-9]{1,3}-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$"
+                                                    title="Phone number must be in the format: +1-123-456-7890 or 123-456-7890"
+                                                />
+                                            </div>
                                             <div style={{ minHeight: "22px" }} className="text-start">
 
                                                 <ErrorMessage name="whatsappNumber" component="div" className="text-danger small" />
@@ -140,7 +220,16 @@ const RegistrationForm = () => {
                                     <div className="row mb-3 align-items-center">
                                         <label htmlFor="email" className="col-sm-3 col-form-label text-start">Email</label>
                                         <div className="col-sm-9">
-                                            <Field name="email" type="email" className={`form-control ${errors.fullName && touched.fullName ? 'input-error' : ''}`} />
+                                            <div className="input-group">
+                                                <span className="input-group-text">
+                                                    <MdEmail />
+                                                </span>
+                                                <Field
+                                                    name="email"
+                                                    type="email"
+                                                    className={`form-control ${errors.email && touched.email ? 'is-invalid' : ''}`}
+                                                />
+                                            </div>
                                             <div style={{ minHeight: "22px" }} className="text-start">
 
                                                 <ErrorMessage name="email" component="div" className="text-danger small" />
